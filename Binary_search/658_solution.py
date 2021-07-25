6ec002a08b6a0d5aa9262a5d4df6206959f0c298
Ab Qyum <qyumjahid@gmail.com>
2021-07-25 23:50:32 +0600

    Update 658_solution.py

--- Binary_search/658_solution.py
+++ Binary_search/658_solution.py
@@ -32,33 +32,21 @@ class Solution(object):
         
         
         low = 0
-        
         high = len(arr) - k
         
         while low < high:
-            
             mid = low + (high-low)//2
-            
             if x<=arr[mid]:
-                
-                high=mid
-                
+                high=mid   
             elif arr[mid+k]<=x:
-                
-                low = mid+1
-                
+                low = mid+1    
             else:
-                
-                middist = abs(x-arr[mid])
-                
+                middist = abs(x-arr[mid]) 
                 midkdist = abs(x-arr[mid+k])
                 
-                if middist <= midkdist:
-                    
-                    high=mid
-                    
-                else:
-                    
+                if middist <= midkdist:  
+                    high=mid     
+                else: 
                     low=mid+1
             
         print(low,k)           
