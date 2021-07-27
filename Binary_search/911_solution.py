ef140c80a18108f384dc901a93b04e8a88c3cfbe
Ab Qyum <qyumjahid@gmail.com>
2021-07-27 16:59:32 +0600

    Create 911_solution.py

---
+++ Binary_search/911_solution.py
@@ -0,0 +1,25 @@
+#bs+hash
+
+class TopVotedCandidate:
+
+    def __init__(self, persons: List[int], times: List[int]):
+        
+        self.time_winning = collections.defaultdict(lambda: -1)
+        vote_count = collections.defaultdict(int)
+        #print(vote_count)
+        cur_max, cur_win = 0, -1
+        
+        for p, t in zip(persons, times):
+            vote_count[p] += 1
+            if vote_count[p] >= cur_max: 
+                cur_max, cur_win = vote_count[p], p
+                print(cur_max,cur_win)
+            self.time_winning[t] = cur_win
+        self.times = times 
+        
+
+    def q(self, t: int) -> int:
+        if t in self.time_winning:
+            return self.time_winning[t]
+        i = bisect.bisect(self.times, t)
+        return self.time_winning[self.times[i-1]]
