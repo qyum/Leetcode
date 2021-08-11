ebedfb9378e58cb60b6cae3c6ccbe4da1b93b5ec
Ab Qyum <qyumjahid@gmail.com>
2021-08-11 19:27:55 +0600

    Create 496_solution.py

---
+++ stack/496_solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        stack = []
+        mapping = defaultdict(lambda: -1)
+
+        for index, element in enumerate(nums2):
+            while stack:
+                top_element = stack[-1]
+                #print(top_element)
+                
+                if element < top_element:
+                    break
+
+                mapping[top_element] = element
+                #print(mapping)
+                stack.pop()
+                
+            stack.append(element)
