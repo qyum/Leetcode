f5c5d46396d1127de2c06bb4afedaf5102927e46
Ab Qyum <qyumjahid@gmail.com>
2021-08-25 16:52:44 +0600

    Create 538_solution.py

---
+++ Tree/538_solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def convertBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        self.helper(root,0)
+        return root
+    
+    def helper(self,root,val):
+        if root is None:
+            return val
+        else:
+            outvalue=self.helper(root.right,val)
+            root.val +=outvalue
+            
+            return self.helper(root.left,root.val)
+          
