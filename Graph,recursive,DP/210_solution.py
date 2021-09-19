e8bde2cf660785067c2ebe97151bcf1176ca5173
Ab Qyum <qyumjahid@gmail.com>
2021-09-19 23:05:42 +0600

    Update 210_solution.py

--- Graph,recursive,DP/210_solution.py
+++ Graph,recursive,DP/210_solution.py
@@ -40,3 +40,46 @@ class Solution:
                     self.result.append(node)
             
         return self.result
+    
+    #......................................2nd approach(topological sort+cycle detection)........................................
+    
+    class Solution:
+        
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        g = {}
+        for i in range(numCourses):
+            g[i] = []
+            
+        for pre in prerequisites:
+            u, v = pre[1], pre[0]
+            g[u].append(v)
+            
+        order = self.topo_sort(g, numCourses)
+        #print(order)
+        return order
+        
+    def topo_sort(self, g, n):
+        visiting, visited, st = set([]), set([]), []
+        for i in range(n):
+            if i not in visited:
+                if self.dfs(g,i,st,visiting, visited):
+                    return []
+        #print(st)
+        st.reverse()
+        return st
+    
+    def dfs(self, g, i, st, visiting, visited):
+        visiting.add(i)
+        
+        for nbr in g[i]:
+            if nbr in visiting:
+                return True
+            elif nbr not in visited:
+                if self.dfs(g, nbr, st, visiting, visited):
+                    return True
+        #print(i)
+        visited.add(i)
+        visiting.remove(i)
+        st.append(i)
+        return False
