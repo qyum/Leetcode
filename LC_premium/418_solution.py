79e263eaea233d4d9f989d09c10f7d6f263c33ef
Ab Qyum <qyumjahid@gmail.com>
2021-12-15 13:51:26 +0600

    Create 418_solution.py

---
+++ LC_premium/418_solution.py
@@ -0,0 +1,17 @@
+#leetcode premium problem(418)
+#sentence screen fitting
+#O(n*m)T | O(1)S
+
+def  wordTyping(sentences,rows,col):
+	joinWord=" ".join(sentences)+" "
+	senLen=len(sentences)
+
+	totalLen=0
+	for rowIdx in range(rows):
+		totalLen += col
+		if joinWord[totalLen % senLen]==" ":
+			totalLen += 1
+		else:
+			while joinWord[(totalLen-1) % senLen] != " " and totalLen>0:
+				totalLen -=1
+	return totalLen//senLen
